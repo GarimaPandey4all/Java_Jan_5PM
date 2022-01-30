b13081b095233848af5af0e0fd06ead64bb5336d
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-01-30 11:43:35 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add {
+    public static void main(String[] args) {
+        
+        int firstNumber = 100;
+        int secondNumber = 200;
+
+        int result = firstNumber + secondNumber;
+
+        System.out.println("Addition is: "+result);
+
+        //System.out.println("Addition is: "+(firstNumber+secondNumber)); // +: concatenation/joining
+
+    }
+}
--- Example.java
+++ Example.java
@@ -2,4 +2,12 @@ class Example {
     public static void main(String[] args) {
         System.out.println("Brain Mentors");
     }
+
+    public static void main() {
+        System.out.println("Java");
+    }
+
+    public static void main(int a) {
+        System.out.println("Example");
+    }
 }
